eb091c72eb61d3882fd1b59be58f28743c6074c6
jiny3 <101464224+jiny3@users.noreply.github.com>
2026-01-13 22:12:27 +0800

    lazyvim update

--- home/.config/nvim/lua/plugins/theme.lua
+++ home/.config/nvim/lua/plugins/theme.lua
@@ -1,5 +1,13 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = { style = "night" },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    config = function()
+      require("github-theme").setup({
+        -- 可以在这里添加自定义选项
+      })
+      vim.cmd("colorscheme github_dark_default")
+    end,
+  },
 }
