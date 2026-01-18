ad1241bc8e05bdf9badbe48a30c4258ed8086f9c
jiny3 <101464224+jiny3@users.noreply.github.com>
2026-01-18 13:07:31 +0800

    update & fix dark mode toggle

--- home/.local/share/noctalia/toggle.sh
+++ home/.local/share/noctalia/toggle.sh
@@ -3,12 +3,10 @@
 # 获取 darkMode 状态
 IS_DARK=$(qs -c noctalia-shell ipc call state all | grep -oP '"darkMode":\s*\K[^,}]*')
 
-if [ "$IS_DARK" = "false" ]; then
-  NOCTALIA_MODE="setDark"
+if [ "$IS_DARK" = "true" ]; then
   BACKGROUND="$HOME/.background-dark"
   PREFER="prefer-dark"
 else
-  NOCTALIA_MODE="setLight"
   BACKGROUND="$HOME/.background"
   PREFER="prefer-light"
 fi
@@ -26,10 +24,5 @@ run_with_retry() {
   return 1
 }
 
-# 执行主题
-run_with_retry "qs -c noctalia-shell ipc call darkMode $NOCTALIA_MODE" &
-
-# 执行壁纸切换
 run_with_retry "qs -c noctalia-shell ipc call wallpaper set $BACKGROUND all"
-
 gsettings set org.gnome.desktop.interface color-scheme $PREFER
