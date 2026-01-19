c9aea8dd0cfaaaada6903f172c4b0fe9e1a55f5a
jiny3 <101464224+jiny3@users.noreply.github.com>
2026-01-19 14:38:57 +0800

    update

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,6 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.wrap = true
-vim.opt.linebreak = false
-vim.opt.breakindent = true
+local opt = vim.opt
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = true
+opt.linebreak = false
+opt.breakindent = true
+
+opt.cursorline = true
+
+opt.mouse:append("a")
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.termguicolors = true
+opt.signcolumn = "yes"
