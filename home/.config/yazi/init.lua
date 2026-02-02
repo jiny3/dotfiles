ab3c704a6ec7461f8a3e15b75961e7067b2962d9
jiny3 <101464224+jiny3@users.noreply.github.com>
2026-02-03 02:43:36 +0800

    theme=catppuccin

--- home/.config/yazi/init.lua
+++ home/.config/yazi/init.lua
@@ -1,26 +1,3 @@
-require("yaziline"):setup({
-	color = "#ff8da1",
-	secondary_color = "#88333c",
-	default_files_color = "darkgray", -- color of the file counter when it's inactive
-	selected_files_color = "white",
-	yanked_files_color = "green",
-	cut_files_color = "red",
-
-	separator_style = "angly", -- "angly" | "curvy" | "liney" | "empty"
-	separator_open = "",
-	separator_close = "",
-	separator_open_thin = " ",
-	separator_close_thin = " ",
-	separator_head = "░▒▓",
-	separator_tail = "▓▒░",
-
-	select_symbol = "",
-	yank_symbol = "󰆐",
-
-	filename_max_length = 24, -- truncate when filename > 24
-	filename_truncate_length = 6, -- leave 6 chars on both sides
-	filename_truncate_separator = "...",
-})
 require("git"):setup({
 	-- Order of status signs showing in the linemode
 	order = 1500,
