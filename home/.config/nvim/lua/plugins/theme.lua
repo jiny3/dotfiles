101d3ebda78664949032b324106cdb89b75b90c1
jiny3 <101464224+jiny3@users.noreply.github.com>
2026-02-03 01:20:12 +0800

    theme=catppuccin

--- home/.config/nvim/lua/plugins/theme.lua
+++ home/.config/nvim/lua/plugins/theme.lua
@@ -1,13 +1,1 @@
-return {
-  {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    lazy = false,
-    config = function()
-      require("github-theme").setup({
-        -- 可以在这里添加自定义选项
-      })
-      vim.cmd("colorscheme github_dark_default")
-    end,
-  },
-}
+return { { "catppuccin/nvim", name = "catppuccin", priority = 1000 } }
