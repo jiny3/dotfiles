57a4be71c1ccfbf3d7ae8905e8b56b566998f5f2
jiny3 <101464224+jiny3@users.noreply.github.com>
2026-02-27 22:56:50 +0800

    only uppercase

---
+++ home/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/flash.nvim",
+    opts = {
+      labels = "asdfghjklqwertyuiopzxcvbnm",
+      label = {
+        uppercase = false,
+      },
+    },
+    keys = { ... }, -- 原有快捷键不变
+  },
+}
